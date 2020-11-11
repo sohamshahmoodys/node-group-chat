269389f7e57db6cfc73018092b4b377489c8d504
sohamshahmoodys <31042040+sohamshahmoodys@users.noreply.github.com>
2020-11-10 22:16:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('chat_message', function(message) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+        console.log('chat_message|'+ socket.username + '|' + message);
     });
 
 });
